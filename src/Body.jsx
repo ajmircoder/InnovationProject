34d38a3c5be9f85b0ea87ec1de467c7529f6122a
ajmir khan <ajmirk01@gmail.com>
2024-02-16 15:32:56 +0530

    without route

---
+++ src/Body.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Login from './components/Login'
+import Product from './components/Product'
+
+function Body() {
+  const [isUser, setIsUser] = useState(false);
+
+  return (
+    <>
+    {isUser ? <Product/> : <Login setIsUser={setIsUser}/>}
+    </>
+  )
+}
+
+export default Body
